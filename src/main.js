bf9b9b9607a88364444d2996b35bfa5d79b0b135
madou9 <issoufou.mamadou@gmail.com>
2025-01-07 21:34:39 +0100

    added use store in the app instance

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 createApp(App)
 .use(router) // Add this line to use the router in the app instance 
+.use(store) // Add this line to use the store in the app instance
 .mount('#app')
