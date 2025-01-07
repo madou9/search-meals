a36c9eea96b4ded3c24ad697f6e2ffb62f908a44
madou9 <issoufou.mamadou@gmail.com>
2025-01-07 22:13:29 +0100

    implemented some functionality that for each letter that user click it will redirect to the letter path

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>
-        <h1>Home</h1>
-        <p>Welcome to the Home page</p>
+    <div class="flex flex-col p-8 ">
+        <input type="text" 
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for meals"
+        />
+        <div class="flex justify-center gap-2 mt-2">
+            <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+                {{ letter }}
+            </router-link>
+        </div>
+
     </div>
 </template>
 
 <script setup>
+    import {computed} from 'vue'
+    import store from '../store/index'
 
+    const meals = computed(() => store.state.meals)
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 </script>
